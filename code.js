217ad4010e4f0b16e9d281a8a9cb13af34a2fb1d
wrxprojektwsb <wrx88035@student.wroclaw.merito.pl>
2025-04-25 21:18:05 +0200

    Add files via upload

---
+++ code.js
@@ -0,0 +1,7 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    const img = document.querySelector('.motivational-image');
+    if(img) {
+        img.style.opacity = 1;
+    }
+});
